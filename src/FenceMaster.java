17eb17ecc6a346ac0640e1e2b8f7e8b4119364ab
James McLaren <jamesmclaren555@gmail.com>
2014-05-27 20:03:29 +1000

    Player now makes 3 moves
    This is just a placeholder until we can make it so that the AI is
    intelligent enough to make proper moves and then we can play them
    against each other

--- src/FenceMaster.java
+++ src/FenceMaster.java
@@ -1,3 +1,5 @@
+import aiproj.fencemaster.Move;
+
 public class FenceMaster {
 
 	boolean replaceRule = true;
@@ -8,11 +10,28 @@ public class FenceMaster {
 	public FenceMaster() {
 
 		player = "Black";
+		
+		Move first = new Move();
+		first.P = 2;
+		first.Row = 1;
+		first.Col = 2;
+		
+		Move second = new Move();
+		second.P = 2;
+		second.Row = 2;
+		second.Col = 0;
 
 		// Initializing a white AI player
 		boardController.init(6, 1);
-		
+		System.out.println("===============================");
 		boardController.makeMove();
+		System.out.println("===============================");
+		boardController.opponentMove(first);
+		boardController.makeMove();
+		System.out.println("===============================");
+		boardController.opponentMove(second);
+		boardController.makeMove();
+		System.out.println("===============================");
 
 	}
 
