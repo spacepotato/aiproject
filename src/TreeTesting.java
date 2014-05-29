21ae85f87813ee62f52b2bb36ab9b38bbd779977
ahowindt <ahowindt@student.unimelb.edu.au>
2014-05-30 02:58:05 +1000

    TreeTesting.java

---
+++ src/TreeTesting.java
@@ -0,0 +1,30 @@
+import aiproj.fencemaster.Move;
+
+
+
+
+public class TreeTesting {
+	
+	public static void main(String args[]){
+		Gameboard gb = new Gameboard();
+		int player = 1;
+		
+		gb.generateHexagons(4);
+
+		gb.updateBoard(new Move(player,false,2,2));
+		gb.updateBoard(new Move(player,false,3,2));
+		gb.updateBoard(new Move(player,false,4,3));
+		gb.updateBoard(new Move(player,false,4,4));
+		gb.updateBoard(new Move(player,false,3,4));
+//		gb.updateBoard(new Move(player,false,2,1));
+		
+		MinMaxTree MMT = new MinMaxTree(gb, player);
+		
+		double value = MMT.tripodEval(gb, player);
+		
+		System.out.println("Value = " + value);
+		
+	}
+	
+	
+}
