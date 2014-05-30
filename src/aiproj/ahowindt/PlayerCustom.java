b265ddb3b01fbae9c9ec51536369f88a4762227f
James McLaren <jamesmclaren555@gmail.com>
2014-05-30 17:52:27 +1000

    Updated player custom

--- src/aiproj/ahowindt/PlayerCustom.java
+++ src/aiproj/ahowindt/PlayerCustom.java
@@ -1,4 +1,15 @@
+/**
+ * Used to aid in the winChecking functionality of our application
+ * @param playerName: String value for player. Black or White
+ * @param playerWin: whether or not this player has won
+ * @param playerWinState: how this player has won. Loop, Tripod or Both
+ * @param playerValue: the integer value corresponding to their string value. 1 or 2 
+ * 
+ * @author AHowindt and JMcLaren
+ */
+
 package aiproj.ahowindt;
+
 public class PlayerCustom {
 
 	protected String playerName;
@@ -8,35 +19,36 @@ public class PlayerCustom {
 
 	public PlayerCustom(String name) {
 		this.playerName = name;
-		
+
 		if(name.equals("Black"))
 			this.playerValue = 2;
 		else
 			this.playerValue = 1;
-		
+
 		this.playerWin = false;
 	}
-	
+
+	//Getters and Setters
 	public void setPlayerWin(boolean win){
 		this.playerWin = win;
 	}
-	
+
 	public boolean getPlayerWin(){
 		return this.playerWin;
 	}
-	
+
 	public void setPlayerWinState(String winState){
 		this.playerWinState = winState;
 	}
-	
+
 	public String getPlayerWinState(){
 		return this.playerWinState;
 	}
-	
+
 	public int getPlayerValue(){
 		return this.playerValue;
 	}
-	
+
 	public void setPlayerValue(int playerValue){
 		this.playerValue = playerValue;
 	}
